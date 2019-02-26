ec99d5667a5470f117d4b8c7f1243a32a6a6cd55
Lahiru Senevirathne <lahiru.16@cse.mrt.ac.lk>
2019-02-26 23:33:18 +0530

    Speech to Text API configured and UI created

--- app.js
+++ app.js
@@ -1,71 +1,78 @@
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 const synth = window.speechSynthesis;
 const recognition = new SpeechRecognition();
+let finalTranscript = '';
+recognition.interimResults = true;
+recognition.maxAlternatives = 10;
+recognition.continuous = true;
 
-const icon = document.querySelector('i.fa.fa-microphone')
+const recordIcon = document.querySelector('#recordButton');
+const stopIcon = document.querySelector('#stopButton');
+const displayIcon = document.querySelector('#displayContent');
 let paragraph = document.createElement('p');
 let container = document.querySelector('.text-box');
 container.appendChild(paragraph);
 const sound = document.querySelector('.sound');
 
-icon.addEventListener('click', () => {
+recordIcon.addEventListener('click', () => {
   sound.play();
   dictate();
 });
 
+stopIcon.addEventListener('click', () => {
+  sound.play();
+  recognition.stop();
+});
+
+displayIcon.addEventListener('click', () => {
+  populatePre('uploads/ReadMe.txt');
+});
+
 const dictate = () => {
+
   recognition.start();
+
   recognition.onresult = (event) => {
-    const speechToText = event.results[0][0].transcript;
+    let interimTranscript = '';
+    for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+      let transcript = event.results[i][0].transcript;
+      if (event.results[i].isFinal) {
+        finalTranscript += transcript;
+      } else {
+        interimTranscript += transcript;
+      }
+    }
+
+
     
-    paragraph.textContent = speechToText;
+    paragraph.textContent = finalTranscript+interimTranscript;
 
  // }}
 
-    if (event.results[0].isFinal) {
 
-      if (speechToText.includes('what is the time')) {
-          speak(getTime);
-      };
-      
-      if (speechToText.includes('what is today\'s date')) {
-          speak(getDate);
-      };
-      
-      if (speechToText.includes('what is the weather in')) {
-          getTheWeather(speechToText);
-      };
-    }
   }
+};
+
+function populatePre(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    // document.getElementById('contents').textContent = this.responseText;
+    paragraph.textContent = this.responseText;
+  };
+  xhr.open('GET', url);
+  xhr.send();
 }
 
-const speak = (action) => {
-  utterThis = new SpeechSynthesisUtterance(action());
-  synth.speak(utterThis);
-};
 
-const getTime = () => {
-  const time = new Date(Date.now());
-  return `the time is ${time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
-};
 
-const getDate = () => {
-  const time = new Date(Date.now())
-  return `today is ${time.toLocaleDateString()}`;
-};
 
-const getTheWeather = (speech) => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${speech.split(' ')[5]}&appid=58b6f7c78582bffab3936dac99c31b25&units=metric`) 
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(weather){
-    if (weather.cod === '404') {
-      utterThis = new SpeechSynthesisUtterance(`I cannot find the weather for ${speech.split(' ')[5]}`);
-      synth.speak(utterThis);
-      return;
-    }
-    utterThis = new SpeechSynthesisUtterance(`the weather condition in ${weather.name} is mostly full of ${weather.weather[0].description} at a temperature of ${weather.main.temp} degrees Celcius`);
-    synth.speak(utterThis);
-  });
-};
+
+// const speak = (action) => {
+//   utterThis = new SpeechSynthesisUtterance(action());
+//   synth.speak(utterThis);
+// };
+
+
+
+
+
