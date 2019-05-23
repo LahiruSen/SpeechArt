f8bca95af892b083e6a8f273ba0e9d44958f2186
Lahiru Senevirathne <lahiru.16@cse.mrt.ac.lk>
2019-05-23 16:51:22 +0530

    Test case added

---
+++ HOST/db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 5/14/2019
+ * Time: 1:36 AM
+ */
---
+++ Testing/db-connect-test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 5/22/2019
+ * Time: 1:03 PM
+ */
